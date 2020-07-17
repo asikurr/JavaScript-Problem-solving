9d153712bac2797f5881276d567dad0c5dd25036
Asikur <asikurrahman7@gmail.com>
2020-07-17 15:52:07 +0600

    Fixed some problem.

--- assignment.js
+++ assignment.js
@@ -1,25 +1,24 @@
 // 1.Problem -  feetToMile
 function feetToMile(feet){
-    if(feet<=0){
+    if(feet<=0){// if value is 0 or negative.
         return "Please enter valid feet number."
     }else{
         let mile = feet/5280; // 5280 feet == 1 mile
-        return feet + " feet = " +mile.toFixed(7) + " Mile.";// using toFixed(7) according to google calculator.
+        return mile.toFixed(7);
     }
 }
-const TotalMile = feetToMile(1);
+const TotalMile = feetToMile(280);
 console.log(TotalMile);
 //input : 280
 // Output :  0.0530303 Mile.
-// Solved Problem 1. feetToMile ///
 
 
 // 2.Problem -  woodCalculator
 function woodCalculator(chair, table , bed){
-    let woodNeedForChair = 1;
-    let woodNeedForTable = 3;
-    let woodNeedForBed = 5;
-    if(chair<=0 || table<=0 || bed<=0){// valided for negative value
+    let woodNeedForChair = 1; //cubic feet
+    let woodNeedForTable = 3; //cubic feet
+    let woodNeedForBed = 5;   //cubic feet
+    if(chair<0 || table<0 || bed<0){// if value is negative.
         return "Enter valid information Like chair, table , bed Number."
     }
     woodNeedForChair = woodNeedForChair * chair;
@@ -28,21 +27,17 @@ function woodCalculator(chair, table , bed){
 
     let Total = woodNeedForChair + woodNeedForTable + woodNeedForBed;
   
-     return chair +" chair needed " + woodNeedForChair +" Cubic feet wood.\n"+ 
-     table +" table needed " + woodNeedForTable +" Cubic feet wood.\n"+
-     bed +" bed needed " + woodNeedForBed +" Cubic feet wood.\n"+
-     "Total "+ chair + " chair, "+ table + " table, " +bed + " bed needs " + Total + " Cubic feet Wood."
+    return Total;
 }
-const woodNeeded = woodCalculator(5,1,1);
+const woodNeeded = woodCalculator(3,6,12);
 console.log(woodNeeded);
 //input : 3,6,12
-// Output :  3,18,60 or 81
-// Solved Problem 2. woodCalculator //
+// Output : 81
 
 
 // 3.Problem -  brickCalculator
 function brickCalculator(floor){
-    if(floor<=0){
+    if(floor<=0){//if input value is 0 or negative number.
         return "Please enter valid floor number.";
     }else if(floor<=10){
         let brickPerFeet = 1000;
@@ -65,14 +60,13 @@ function brickCalculator(floor){
         return totlaBrickNeedAboveFloor;
     }
 }
-const totalBrick = brickCalculator(2);
+const totalBrick = brickCalculator(5);
 console.log(totalBrick);
-//input : 5,15,25
-// Output :  75000,210000,320000
-// Solved Problem 3. brickCalculator //
+//input : 5/15/25
+// Output :  75000/210000/320000
 
-// 4.Problem -  tinyFriends
-function tinyFriends(friends){
+// 4.Problem -  tinyFriend
+function tinyFriend(friends){
     let minLengthName = friends[0];
     if(minLengthName.length ==  ""){ //if friends Array is empty [""]
         return "Enter your friend name."
@@ -83,11 +77,10 @@ function tinyFriends(friends){
             minLengthName = frindsName;
         }
     }
-    return "Your tinyFriend Name is : "+minLengthName;
+    return minLengthName;
 }
 const friends = ["Asikur","Rahaman", "Rafi", "Farhan"]; 
-console.log(tinyFriends(friends))
+console.log(tinyFriend(friends))
 //input : ["Asikur","Rahaman", "Rafi", "Farhan"]
 // Output :  Rafi
-// Solved Problem 4. tinyFriends //
 
